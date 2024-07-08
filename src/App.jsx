9628f1516ab271805f14e2a1f4f5f0ac2069f938
Ajay Kumar Maurya <ajaykm000563@gmail.com>
2024-07-08 13:07:15 +0530

    Your meaningful commit message

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             <Route path='/about' element={<About/>}/>
             <Route path='/contact' element={<Contact/>}/>
             <Route path='/menu' element={<Menu/>}/>
-            <Route path='*' element={<PageNotFound/>}/>
+            <Route path='*' element={<Home/>}/>
          </Routes>
       </BrowserRouter>
     </>
